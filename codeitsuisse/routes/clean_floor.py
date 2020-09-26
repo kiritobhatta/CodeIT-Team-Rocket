6f28537d246ca11bd9933d6a2231d11834f59c5f
Umakant <umakantbhatt123@gmail.com>
2020-09-26 19:12:51 +0800

    Update clean_floor - 1.3

--- codeitsuisse/routes/clean_floor.py
+++ codeitsuisse/routes/clean_floor.py
@@ -14,22 +14,48 @@ def evaluate_clean_floor():
     tests = data.get("tests");
     ans = {}
     for key in tests:
-        ans[key] = clean_floor(tests[key]['floor'])
+        ans[key] = clean_floor1(tests[key]['floor'])
 
     logging.info("My result :{}".format(ans))
     return json.dumps({"answers": ans})
 
 
-
-
+def update_array(array, start, end, flag):
+    for count in range(end - start):
+        if not flag:
+            index = start + count + 1
+        else:
+            index = end - count - 1
+        if array[index] > 0:
+            array[index] -= 1
+        else:
+            array[index] += 1
+    return array
+    
 def clean_floor1(list_array):
     start_index = 0
     end_index = len(list_array) - 1
-
-    
-
-
-
+    flag = False
+    moves = 0
+    while start_index != end_index:
+        if flag:
+            while list_array[start_index] == 0 and start_index <= len(list_array) - 1 and start_index != end_index:
+                start_index += 1
+        else:
+            while list_array[end_index] == 0 and end_index >= 0 and start_index != end_index:
+                end_index -= 1
+        list_array = update_array(list_array, start_index, end_index, flag)
+        moves += end_index - start_index
+        flag = not flag
+            
+    if list_array[start_index] != 0:
+        print("fuck")
+        if list_array[start_index] % 2 == 0:
+            moves += list_array[start_index] * 2 + 1
+        else:
+            moves += (list_array[start_index]+1) * 2
+                
+    return moves
 
 
 
