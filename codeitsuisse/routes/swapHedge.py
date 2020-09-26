d801e2c30069c97ffb9647588d31de3197598f8d
Umakant <umakantbhatt123@gmail.com>
2020-09-27 06:54:53 +0800

    Update swapHedge - 1.1

--- codeitsuisse/routes/swapHedge.py
+++ codeitsuisse/routes/swapHedge.py
@@ -13,11 +13,12 @@ def evaluate_swapHedge():
     logging.info("data sent for evaluation {}".format(data))
     previous = 0
     output = 0
+    dict_new = {}
     # for test_cases in data:
-    output = data.get("accu_order") - previous
+    dict_new['output'] = data.get("accu_order") - previous
     previous = data.get("accu_order")
 
-    logging.info("result : {}".format(output))
-
-    return json.dumps({'output': int(output)})
+    logging.info("output : {}".format(dict_new['output']))
+    
+    return jsonify(dict_new)
 
