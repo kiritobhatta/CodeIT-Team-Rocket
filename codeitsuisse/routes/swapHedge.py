62957fc44ec725b3e389202f1c187a7d34cc51eb
Umakant <umakantbhatt123@gmail.com>
2020-09-27 07:11:35 +0800

    Update yinYang - 1.2

--- codeitsuisse/routes/swapHedge.py
+++ codeitsuisse/routes/swapHedge.py
@@ -11,14 +11,12 @@ logger = logging.getLogger(__name__)
 def evaluate_swapHedge():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    previous = 0
-    output = 0
+    
     dict_new = {}
     # for test_cases in data:
-    dict_new['output'] = data.get("accu_order") - previous
-    previous = data.get("accu_order")
+    dict_new['output'] = data.get("order")
 
     logging.info("output : {}".format(dict_new['output']))
-    
+
     return jsonify(dict_new)
 
--- codeitsuisse/routes/yinYang.py
+++ codeitsuisse/routes/yinYang.py
@@ -8,54 +8,84 @@ from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
 
-def compare(counter, bitmask, value):
-    temp = bitmask
+def check(i,bitmask,elements):
+    b = bitmask
     count = 0
-    counter += 1
-    while counter>0:
-        if(temp%2==1):
-            counter-=1
-            if(counter==0):
+    # print(i)
+    i += 1
+    while i>0:
+        if(b%2==1):
+            i-=1
+            if(i==0):
                 break
         count+=1
-        temp = temp >> 1   
-       
-    return (value[len(value)-1-count] == 'Y', count)    
+        b = b >> 1   
+    # print(elements, " --> ", count, elements[len(elements)-1-count])    
+    return (elements[len(elements)-1-count] == 'Y', count)    
 
-def findExpected(num_values, num_operations, values):
-    poss = [0] * num_operations
-    total = [0] * num_operations
+def ev(bitmask, elements):
+    s = ""
+    b = bitmask
+    i = 0
+    # print("---------")
+    while b >= 1:
+        # print(b)
+        if(b%2 == 1):
+            s += elements[len(elements) - 1 - i]
+        i += 1
+        b = b >> 1
+    # print(s,bin(bitmask),elements,"<----")    
+    return s        
+
+def findExpected(number_of_elements, number_of_operations, elements):
+    poss = [0] * (number_of_operations + 5)
+    total = [0] * (number_of_operations + 5)
+    map = {}
     q = Queue()
-    q.put(((1<<num_values)-1, 0))
+    q.put(((1<<number_of_elements)-1, 0))
     while(not q.empty()):
         fr = q.get()
         bitmask = fr[0]
         numOps = fr[1]
-        
-        if(numOps >= num_operations):
-            break
-        for i in range(num_values-numOps):
-            
-            poss[numOps] += 1
-            total[numOps] += 1
-            (cond, pos) = compare(i, bitmask, values)
-            
-            if cond:
-                q.put((bitmask & ~(1<<pos), fr[1] + 1))
-            else:
-                (cond, pos) = compare(num_values - numOps - i - 1, bitmask, values)
+        shrt = ev(bitmask, elements)
+        # print(shrt, map.keys())
+        if shrt in map.keys():
+            # print(shrt, map[shrt])
+            poss[numOps] += map[shrt][0]
+            total[numOps] += map[shrt][1]
+        else:
+            # print(bin(bitmask), numOps)
+            p = 0
+            t = 0
+            if(numOps >= number_of_operations):
+                break
+            for i in range(number_of_elements-numOps):
+                # print(i,"--", numOps)
+                poss[numOps] += 1
+                p += 1
+                total[numOps] += 1
+                t += 1
+                (cond, pos) = check(i,bitmask,elements)
+                # print(elements[len(elements)-1-pos], bin(bitmask), i)
                 if cond:
                     q.put((bitmask & ~(1<<pos), fr[1] + 1))
                 else:
-                    poss[numOps] -= 1
-                    
+                    (cond, pos) = check(number_of_elements - numOps - i -1,bitmask,elements)
+                    if cond:
+                        q.put((bitmask & ~(1<<pos), fr[1] + 1))
+                    else:
+                        poss[numOps] -= 1
+                        p -= 1
+            map[shrt] = (p,t)        
 
     expected = 0
-    for i in range(num_operations):
+    # print(poss)
+    # print(total)
+    for i in range(number_of_operations):
         if total[i]:
             expected += poss[i] / total[i]
 
-    return expected  
+    return expected    
 
 @app.route('/yin-yang', methods=['POST'])
 def evaluate_yinYang():
