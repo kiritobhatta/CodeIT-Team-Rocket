603a3662b16d82f9904761d912be660e7b3d9168
Umakant <umakantbhatt123@gmail.com>
2020-09-27 01:14:24 +0800

    Update magicalBasket - 1.8

--- codeitsuisse/routes/magicalFruitBasket.py
+++ codeitsuisse/routes/magicalFruitBasket.py
@@ -16,8 +16,8 @@ def evaluateFruitbasket():
     logging.info("data sent for evaluation2 {}".format(data))
     # print("keys:",data.keys)
     weight1 = 11
-    weight2 = 27
-    weight3 = 32
+    weight2 = 26
+    weight3 = 31
 
     listNo = []
     for key in data.keys():
