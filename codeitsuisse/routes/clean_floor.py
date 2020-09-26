38dc627dfc9088b4a52339c7df480ff494c80f7e
Umakant <umakantbhatt123@gmail.com>
2020-09-26 18:08:41 +0800

    Update clean_floor - 1.2

--- codeitsuisse/routes/clean_floor.py
+++ codeitsuisse/routes/clean_floor.py
@@ -17,34 +17,49 @@ def evaluate_clean_floor():
         ans[key] = clean_floor(tests[key]['floor'])
 
     logging.info("My result :{}".format(ans))
-    return json.dumps({"answers": ans});
+    return json.dumps({"answers": ans})
+
+
+
+
+def clean_floor1(list_array):
+    start_index = 0
+    end_index = len(list_array) - 1
+
+    
+
+
+
+
 
 
 def clean_floor(list_array):
     start_index = 0
     current_index = 0
     moves = 0
+
     if (len(list_array) == 1):
         moves = list_array[0]
     else:
         while (start_index != len(list_array)):
-            if (current_index >= len(list_array)):
-                current_index = start_index - 1
-            else:    
-                current_index = start_index + (current_index + 1) % 2
-            if (list_array[current_index] != 0 or list_array[start_index]):
-                moves += 1
-                if list_array[current_index] > 0:
-                    list_array[current_index] -= 1
+            
+            if current_index != start_index:
+                current_index = start_index
+            else:
+                if (current_index >= len(list_array)-1):
+                    current_index = start_index - 1
                 else:
-                    list_array[current_index] += 1
+                    current_index = start_index + 1
+            print(current_index)
+            moves += 1
+            if list_array[current_index] > 0:
+                list_array[current_index] -= 1
+            else:
+                list_array[current_index] += 1
 
-            if start_index != current_index:
-                if (list_array[start_index] == 0 and start_index != len(list_array)-1):
-                    start_index += 1
-                    current_index += 1
-                elif start_index == len(list_array)-1 and list_array[start_index] == 0 and list_array[current_index] == 0:
-                    start_index +=1
-                    current_index += 1
-
+            if start_index >= len(list_array)-2 and list_array[start_index] == 0 and list_array[current_index] == 0:
+                break
+            elif (list_array[start_index] == 0 and start_index != len(list_array)-1):
+                start_index += 1
+        
     return moves
