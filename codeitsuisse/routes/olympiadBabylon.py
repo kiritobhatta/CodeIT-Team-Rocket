62f575648d7649be63313d1b1aefebc0a1f6176f
Umakant <umakantbhatt123@gmail.com>
2020-09-27 02:41:27 +0800

    Update olympiadBabylon - 1.1

--- codeitsuisse/routes/olympiadBabylon.py
+++ codeitsuisse/routes/olympiadBabylon.py
@@ -8,52 +8,77 @@ from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
 
-def num_books_for_min(books_left, minute, books_read):
-    if len(books_left) == 1:
-        if books_left[0] > minute:
-            return [books_read]
-        else:
-            return [books_read + books_left]
-    if books_left[0] <= minute < books_left[1]:
-        return [books_read + books_left[:1]]
-    all_books_sets = [books_read]
-    max_books = len(books_read)
-    for i in range(len(books_left) - 1):
-        if books_left[i] <= minute:
-            books = num_books_for_min(books_left[i + 1:], minute - books_left[i], books_read + [books_left[i]])
-            if len(books[0]) > max_books:
-                max_books = len(books[0])
-                all_books_sets = books
-            elif len(books[0]) == max_books:
-                all_books_sets += books
-    return all_books_sets
+def olympiad(days,books):
+    import math
+    days = sorted(days, reverse=True)
+    # dimensions of dp:
+    #   index reached so far
+    #       leftover so far -> length, [nums]
+    target = days[0]
+    answer = 0
+    while days:
+        target = days.pop()
+        # print(books)
+        dp = {}
+        for x in range(len(books)+1):
+            dp[x] = {}
 
+        dp[0][target] = (0, [])
 
-def max_num_books(books_left, days):
-    max_ans = 0
-    for minute in days:
-        books = num_books_for_min(books_left, minute, [])
-        max_books = books[-1]
-        max_ans += len(max_books)
-        for i in max_books:
-            books_left.remove(i)
-    return max_ans
+        for index, element in enumerate(books):
+            index += 1
+            for sub_indice in range(index):
+                for sub_leftover in dp[sub_indice]:
+                    sub_len, sub_elems = dp[sub_indice][sub_leftover]
+                    cur_leftover = sub_leftover - element
+                    if cur_leftover >= 0:
+                        if cur_leftover not in dp[index]:
+                            dp[index][cur_leftover] = (
+                                sub_len+1, sub_elems+[index-1])
+                        else:
+                            if sub_len+1 < dp[index][cur_leftover][0]:
+                                dp[index][cur_leftover] = (
+                                    sub_len+1, sub_elems+[index-1])
+        min_val = math.inf
+        output = []
+        # for indice, leftovers in dp.items():
+        #     print()
+
+        # print(sorted(, reverse=True))
+        max_val_count = -math.inf
+        min_val_leftovers = math.inf
+        for index in dp:
+            for leftovers in dp[index]:
+                count = dp[index][leftovers][0]
+                if count > max_val_count:
+                    max_val_count = count
+                    min_val_leftovers = leftovers
+                    indices = dp[index][leftovers][1]
+                elif count == max_val_count:
+                    if leftovers < min_val_leftovers:
+                        indices = dp[index][leftovers][1]
+                        min_val_leftovers = leftovers
+
+        answer += max_val_count
+        # print(max_val_count, min_val_leftovers, indices)
+        new_arr = []
+        x = 0
+        for ind, elem in enumerate(books):
+
+            if x < len(indices) and ind == indices[x]:
+                x += 1
+            else:
+                new_arr.append(elem)
+
+        books = new_arr
+    return answer
 
 @app.route('/olympiad-of-babylon', methods=['POST'])
-def evaluateBabylon():
+def evaluateOB():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    no_books = data.get("numberOfBooks")
-    no_days = data.get("numberOfDays")
-
-    books_left = sorted(data.get("books"))
-    days = sorted(data.get("days"), reverse=True)
-
-    max_ans = max(
-        max_num_books(books_left.copy(), days),
-        max_num_books(books_left.copy(), days[::-1])
-    )
-    result = {"optimalNumberOfBooks": max_ans}
-
+    days = data["days"]
+    books = data["books"]
+    result = {"optimalNumberOfBooks" : olympiad(days,books)}
     logging.info("My result :{}".format(result))
-    return jsonify(result)
+    return json.dumps(result)
